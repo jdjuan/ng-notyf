616d2d0a709027b2d8ffb4ec1fa87ac66f8dee99
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2017-04-16 02:16:52 -0500

    chore(gulpfile): add templates and styles inlining into components

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sample-component',
-  template: `<h1>Sample component</h1>`
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css']
 })
 export class SampleComponent {
 
