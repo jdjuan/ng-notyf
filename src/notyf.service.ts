f05774da14ff251c38135686fab9669f3924aa72
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2017-04-24 21:43:15 -0500

    feat: add notyf service

---
+++ src/notyf.service.ts
@@ -0,0 +1,31 @@
+import { 
+  Injectable, 
+  ApplicationRef,
+  Injector,
+  ComponentFactoryResolver, 
+  EmbeddedViewRef,
+  ComponentFactory } from '@angular/core';
+
+import { ToastComponent } from './toast.component';
+
+@Injectable()
+export class NotyfService {
+
+  factory: ComponentFactory<ToastComponent>;
+  toastContainer: HTMLElement;
+
+  constructor(private appRef: ApplicationRef,
+              componentFactoryResolver: ComponentFactoryResolver,
+              private injector: Injector) { 
+    this.factory = componentFactoryResolver.resolveComponentFactory(ToastComponent);
+    this.toastContainer = document.createElement('div');
+    this.toastContainer.classList.add('toast-container');
+    document.body.appendChild(this.toastContainer);
+  }
+
+  addComponent() {
+    let componentRef = this.factory.create(this.injector);
+    this.appRef.attachView(componentRef.hostView);
+    this.toastContainer.appendChild((componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement)
+  }
+}
---
+++ src/toast.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.css']
+})
+export class ToastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
