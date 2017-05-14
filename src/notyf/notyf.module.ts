77436ed51cd8d5142deaeb7110ddcaa0a829bc09
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2017-05-14 15:53:33 -0500

    feat: add toast animations

--- src/notyf/notyf.module.ts
+++ src/notyf/notyf.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DomService } from './shared/dom.service';
 import { ToastContainerComponent } from './toast-container/toast-container.component';
 import { NgModule } from '@angular/core';
@@ -8,7 +9,7 @@ import { NotyfService } from './shared/notyf.service';
 export { NotyfService } from './shared/notyf.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserAnimationsModule],
   declarations: [ToastComponent, ToastContainerComponent],
   exports: [],
   entryComponents: [ToastComponent, ToastContainerComponent],
--- src/notyf/toast/toast.component.ts
+++ src/notyf/toast/toast.component.ts
@@ -1,3 +1,5 @@
+import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser/platform-browser';
+import { OpaqueToken } from '@angular/core/core';
 import {
   Component,
   OnInit,
@@ -6,9 +8,38 @@ import {
   Renderer
 } from '@angular/core';
 
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
+
+export const flyInOut = trigger('flyInOut', [
+  transition('void => *', [
+    style({
+      transform: 'translateY(0) scaleX(0.75)',
+      marginBottom: '-50px',
+      opacity: 0
+    }),
+    animate('0.4s cubic-bezier(0.23, 1, 0.32, 1)')
+  ]),
+  transition('* => void', [
+    animate('0.4s cubic-bezier(0.23, 1, 0.32, 1)', style({
+      transform: 'translateX(-10px)',
+      opacity: 0
+    }))
+  ])
+])
+
 @Component({
   selector: 'notyf-toast',
   template: `{{message}}`,
+  animations: [flyInOut],
+  host: {
+    '[@flyInOut]': 'in'
+  },
   styleUrls: ['./toast.component.css']
 })
 export class ToastComponent implements OnInit {
